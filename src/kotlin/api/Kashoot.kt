deafaeafdb257696d77b3700d94babb249a5ed02
PotatoCurry <potatocurry@protonmail.com>
2019-05-18 21:19:37 -0500

    Fix retardation

--- src/kotlin/api/Kashoot.kt
+++
@@ -1,9 +0,0 @@
-package io.github.potatocurry.kashoot.api
-
-internal const val kahootEndpoint = "https://play.kahoot.it/rest/kahoots"
-
-class Kashoot {
-    fun getQuiz(quizID: String): Quiz {
-        return Quiz(quizID)
-    }
-}
